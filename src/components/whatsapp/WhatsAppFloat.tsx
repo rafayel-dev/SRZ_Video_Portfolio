0c66e68f39547577820faadd71029b4a9270ac9a
Mr Professor <rafayelislam9@gmail.com>
2025-12-24 11:03:25 +0600

    s

--- src/components/whatsapp/WhatsAppFloat.tsx
+++ src/components/whatsapp/WhatsAppFloat.tsx
@@ -11,19 +11,16 @@ const WhatsAppFloat: React.FC = () => {
 
   const phoneNumber = data.data;
 
-
   // const phoneNumber = "8801751876070";
-  const message = "Hello, I need some help";
+  const message = "Hello, I need some help!";
 
   const whatsappLink = `https://wa.me/${phoneNumber.phone}?text=${encodeURIComponent(
     message)}`;
 
-  
-
   return (
-    <div className="fixed bottom-6 right-6 z-50 group flex items-center">
+    <div className="fixed bottom-10 right-8 z-50 group flex items-center">
       {/* Hover Text */}
-      <span className="mr-3 bg-white text-black text-sm px-3 py-1 rounded opacity-0 translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300">
+      <span className="mr-2 bg-white text-black text-sm px-2 py-1 rounded opacity-0 translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300">
         Chat on WhatsApp
       </span>
 
